3a6b78d02c7340d0a0b7825546627b8e4dc194c1
qianzhongxiang <qianzhongxiang@outlook.com>
2018-11-26 14:15:02 +0800

    input bug

--- projects/vinci-ng-material/src/lib/component/dataTable/table/table.module.ts
+++ projects/vinci-ng-material/src/lib/component/dataTable/table/table.module.ts
@@ -5,6 +5,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatTableModule,
     MatPaginatorModule,
     MatCheckboxModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [TableComponent],
   exports: [TableComponent]
